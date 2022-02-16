d06798019e8c738661c1f505072bf8cf43c180e6
CharlieWWW94 <cdwhiteside@gmail.com>
2022-02-16 11:48:24 +0000

    Started to addd basic form of webpage and created form to submit request information. Now using environment variables for secret key.

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
 from flask import Flask, render_template, request, jsonify
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import SubmitField, SelectField
+import os
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+Bootstrap(app)
 
 
-@app.route("/")
+class SearchForm(FlaskForm):
+    theme = SelectField('Theme', choices=['Ambition', 'Witchcraft', 'Equivocation'])
+    character = SelectField('Character', choices=['Macbeth', 'Lady Macbeth', 'First Witch'])
+    act = SelectField('Act', choices=[1, 2])
+    scene = SelectField('Scene', choices=[1, 2, 3, 4, 5])
+    submit = SubmitField('Submit')
+
+
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return render_template("index.html")
+    form = SearchForm()
+    if form.validate_on_submit():
+        list_search_params = list(form.data.items())
+        print(list_search_params)
+
+    return render_template("index.html", form=form)
 
 
 if __name__ == "__main__":
