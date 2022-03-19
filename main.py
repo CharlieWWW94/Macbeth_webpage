1718eed0af402f351198b91f7c1590a4a2c45737
CharlieWWW94 <cdwhiteside@gmail.com>
2022-03-19 16:48:42 +0000

    On submit, quotation guesses are compared with correct answer and console prints 'correct' if guess and original are equal. Improve learn_quotations.html design to fit with results page and website theme.

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template, request, jsonify, session
+import copy
+import json
+
+from flask import Flask, render_template, request, jsonify, session, flash
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import SubmitField, SelectField
 import os
 import api_communicator
 import random
+import ast
 import sys
 
 app = Flask(__name__)
@@ -38,23 +42,36 @@ def display_results(results):
     return render_template("search_results.html", results=results)
 
 
-@app.route("/learn_quotations", methods=["GET"])
+@app.route("/learn_quotations", methods=["GET", "POST"])
 def learn_quotations():
     quotations_to_learn = session.get('quotations', None)
     for quotation in quotations_to_learn:
         quotation_as_list = quotation['quotation'].split()
         quotation['quotation'] = quotation_as_list
 
+    quotations_to_edit = copy.deepcopy(quotations_to_learn)
     quotations_to_complete = {'quotations': []}
-    for quotation in quotations_to_learn:
+
+    for quotation in quotations_to_edit:
         to_remove = random.randint(0, int(len(quotation['quotation']) - 1))
         quotation_to_complete = quotation
         quotation_to_complete['quotation'][to_remove] = 'X'
-        print(quotation_to_complete)
         quotations_to_complete['quotations'].append(quotation_to_complete)
-    print(quotations_to_complete)
 
-    return render_template("learn_quotations.html", quotations=quotations_to_complete)
+    if request.method == 'POST':
+        request_info = request.values.to_dict()
+        print(request_info)
+        request_info_as_dict = ast.literal_eval(request_info['quotation_no'])
+        quotation_id = request_info_as_dict['id']
+        request_quotation_list = request_info_as_dict['quotation']
+        word_gap = request_quotation_list.index("X")
+
+        original_quotation = [quote for quote in quotations_to_learn if quote['id'] == quotation_id]
+        print(original_quotation[0]['quotation'][word_gap])
+
+        if original_quotation[0]['quotation'][word_gap] == request_info['filled_gap']:
+            print('correct')
+    return render_template("learn_quotations.html", quotations=quotations_to_complete, space=" ")
 
 
 if __name__ == "__main__":
